d5f40cb9e521b6dbb425ae40addd9e02521aae55
NetzkroneHD <noah_01@gmx.de>
2021-09-29 18:33:58 +0200

    Improved system

--- src/main/java/de/noah/infoha/Info.java
+++ src/main/java/de/noah/infoha/Info.java
@@ -1,15 +1,11 @@
 package de.noah.infoha;
 
-import de.noah.infoha.verschluesselung.Caesar;
-import de.noah.infoha.verschluesselung.Verschluesselung;
+import de.noah.infoha.netzwerk.ConsolePanel;
 
 public class Info {
 
     public static void main(String[] args) {
-        final Verschluesselung caesar = new Caesar(2);
-
-        System.out.println(caesar.codiere("INFORMATIK"));
-        System.out.println(caesar.decodiere(caesar.codiere("INFORMATIK")));
-
+        final ConsolePanel cp = new ConsolePanel();
+        cp.setVisible(true);
     }
 }
